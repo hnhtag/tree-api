0266d254b258640c829daab5b7b6e69c95406381
hnhtag <heinhtaung@gmail.com>
2025-05-27 23:01:24 +0630

    chore: db

---
+++ TreeApi/Migrations/20250527160744_Init.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TreeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TreeNodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ParentId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreeNodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TreeNodes_TreeNodes_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "TreeNodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TreeNodes_ParentId",
+                table: "TreeNodes",
+                column: "ParentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TreeNodes");
+        }
+    }
+}
